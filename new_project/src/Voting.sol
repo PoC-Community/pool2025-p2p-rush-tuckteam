0084271d926228cb26461b21c7181822122bc69e
ulysse <ulysse.couchoud@epitech.eu>
2025-02-08 11:06:17 +0100

    [ADD] - forge init and variables / structures and contructor for voting

---
+++ new_project/src/Voting.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.8.20;
+
+contract Voting {
+    struct Voter {
+        bool voted;  // if true, that person already voted
+        uint vote;   // index of the voted proposal
+    }
+
+    struct Proposal {
+        bytes32 name; // short name
+        uint voteCount; // number of accumulated votes
+    }
+
+    address public owner;
+
+    // This declares a state variable that
+    // stores a `Voter` struct for each possible address.
+    mapping(address => Voter) public voters;
+
+    // A dynamically-sized array of `Proposal` structs.
+    Proposal[] public proposals;
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
+
+    constructor(bytes32[] memory proposalName) {
+        owner = msg.sender;
+
+        for (uint i = 0; i < proposalName.length; i++){
+            proposals.push(Proposal(proposalName[i], 0));
+        }
+    }
+}
