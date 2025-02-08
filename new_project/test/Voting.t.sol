d775f4fee673622f5484aee8ff9f366112297915
ulysse <ulysse.couchoud@epitech.eu>
2025-02-08 12:24:36 +0100

    [ADD] - tests for constructor

---
+++ new_project/test/Voting.t.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.20;
+import "../src/Voting.sol";
+import "../lib/forge-std/src/Test.sol";
+
+
+contract VotingTest is Test {
+    Voting voting;
+    string[] array = ["Donald Trump", "Kamala Haaris", "Ulysse Couchoud"];
+    uint256 time = 13 hours;
+
+    function setUp() public {
+        voting = new Voting(array, time);
+    }
+
+    function testConstructor() public view {
+        for (uint i = 0; i < voting.getProposalLength(); i++){
+            (string memory proposalName, ) = voting.proposals(i);
+            assertEq(proposalName, array[i], "Name is not the same");
+        }
+        assertEq(voting.endTime(), block.timestamp + time, "Time is not correct");
+    }
+}
